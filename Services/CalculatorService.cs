946a327542322361507198e1b85b4075688feaa4
huangsheng <huangsheng@lenovohit.com>
2018-11-05 11:28:20 +0800

    实体传参

--- Services/CalculatorService.cs
+++ Services/CalculatorService.cs
@@ -25,44 +25,44 @@ namespace Service
             return _result;
         }
         //通过Id获取用户
-        public DataSet GetUserInfoById(int id)
+        public DataSet GetUserInfoById(EntityAdmin entity)
         {
-            string sql = "select * from Admin where Id=" + id;
+            string sql = "select * from Admin where Id=" + entity.id;
             var _result = SQLHelper.GetAdmin(sql);
             return _result;
         }
         //新增
-        public bool SaveUser(string username,string userpwd)
+        public bool SaveUser(EntityAdmin entity)
         {
-            string sql = "Insert Admin(UserName,UserPwd,Name,Sex,Tel)" + "values('" + username + "','" + userpwd + "','User','男','12345678900')";
+            string sql = "Insert Admin(UserName,UserPwd,Name,Sex,Tel)" + "values('" + entity.username + "','" + entity.userpwd + "','User','男','12345678900')";
             return SQLHelper.ExcuteSql(sql);
            
         }
         //删除
-        public bool DeleteUser(int id)
+        public bool DeleteUser(EntityAdmin entity)
         {
-            string sql = "delete from Admin where Id="+id;
+            string sql = "delete from Admin where Id="+entity.id;
             return SQLHelper.ExcuteSql(sql);
 
         }
         //修改保存
-        public bool UpdateUser(string username, string userpwd,int id)
+        public bool UpdateUser(EntityAdmin entity)
         {
-            string sql = "Update Admin Set UserName="+username+",UserPwd="+userpwd+"where Id="+id+"";
+            string sql = "Update Admin Set UserName="+entity.username+",UserPwd="+entity.userpwd+"where Id="+entity.id+"";
             return SQLHelper.ExcuteSql(sql);
 
         }
-       public DataSet ByUserNameUser(string name)
+       public DataSet ByUserNameUser(EntityAdmin entity)
         {
-            string sql = "select * from Admin where UserName="+name;
+            string sql = "select * from Admin where UserName="+entity.username;
             return SQLHelper.GetAdmin(sql);
         }
 
         //模糊查询
-        public DataSet ByUserPwdAndUserName(string name, string pwd)
+        public DataSet ByUserPwdAndUserName(EntityAdmin entity)
         {
             
-            string sql = "select * from Admin where UserName Like '%"+name+"%' and  UserPwd Like '%" + pwd + "%'";
+            string sql = "select * from Admin where UserName Like '%"+entity.username+"%' and  UserPwd Like '%" + entity.userpwd + "%'";
             return SQLHelper.GetAdmin(sql);
         }
     }
